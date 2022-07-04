b1e8748d100b1e76389420a965105692adece52d
maeng6 <smalll0302@gmail.com>
2022-07-04 22:29:02 +0900

    second commit

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a13c1d3e4c125c9756fbb992727d7d28",
-"index.html": "e0dd71c991fe88242fc104b70f3466f7",
-"/": "e0dd71c991fe88242fc104b70f3466f7",
-"main.dart.js": "25dc307219a9e31219c8d19be0c316d6",
+"index.html": "1accb319006f105579e5f1af459c435b",
+"/": "1accb319006f105579e5f1af459c435b",
+"main.dart.js": "118b901e0667fa7aa0350678893c975a",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
